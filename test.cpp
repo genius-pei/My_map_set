fd475157bc4ad9002b09a4d6f6aa76e5072e01cb
Yiming <2018565194@qq.com>
2025-10-17 17:05:57 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,8 +10,26 @@ void test01()
 	s.insert(6);
 	s.insert(2);
 }
+void test02()
+{
+	yiming::set<int> s;
+	s.insert(4);
+	s.insert(22);
+	s.insert(211);
+	s.insert(2);
+	s.insert(1);
+	yiming::set<int>::iterator it = s.begin();
+	while (it !=s.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+}
 int main()
 {
-	test01();
+	//test01();
+	test02();
 	return 0;
+		 
 }
