14b2064ded360e5c9f0b06af49f12750814e4705
Yiming <2018565194@qq.com>
2025-10-18 21:52:27 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,7 +15,7 @@ void test02()
 	yiming::set<int> s;
 	s.insert(4);
 	s.insert(22);
-	s.insert(211);
+	s.insert(21);
 	s.insert(2);
 	s.insert(1);
 	yiming::set<int>::iterator it = s.begin();
@@ -26,10 +26,34 @@ void test02()
 	}
 	cout << endl;
 }
+void test03()
+{
+	yiming::map<string, string> dict;
+	dict.insert({ "sort", "排序" });
+	dict.insert({ "left", "左边" });
+	dict.insert({ "right", "右边" });
+
+	dict["left"] = "左边，剩余"; // 修改
+	dict["insert"] = "插入";  // 插入+修改
+	dict["string"];          // 插入
+
+	yiming::map<string, string>::iterator it = dict.begin();
+	while (it != dict.end())
+	{
+		// 不能修改first，可以修改second
+		//it->first += 'x';
+		it->second += 'x';
+		cout << it->first << ":" << it->second << endl;
+		++it;
+	}
+	cout << endl;
+}
 int main()
 {
 	//test01();
-	test02();
+	//test02();
+	test03();
+	
 	return 0;
 		 
 }
